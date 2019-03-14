6ba32fc42427cef1f340877227b9a0865c7aebf4
Eduardo Raider <eduraider@gmail.com>
2019-03-14 17:34:09 -0300

    Flutter Contact List

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contact_list/ui/home_page.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
